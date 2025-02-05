5e404252a0d6c9ab6a21c07f47eb88a3f4e0613f
yohan-ban <yohan1312@kookmin.ac.kr>
2025-02-05 14:22:18 +0900

    update After plot (fps)

--- tools/utils/visualize.py
+++ tools/utils/visualize.py
@@ -80,9 +80,6 @@ def plot_tracking(image, bbox_margin_w, bbox_margin_h, xyxy, tlwhs, obj_ids, arg
 
     radius = max(5, int(im_w/140.))
     
-    cv2.putText(im, 'frame: %d fps: %.2f num: %d' % (frame_id, fps, len(tlwhs)),
-                (0, int(15 * text_scale)), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), thickness=2)
-    
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
@@ -94,22 +91,27 @@ def plot_tracking(image, bbox_margin_w, bbox_margin_h, xyxy, tlwhs, obj_ids, arg
         intbox = tuple(map(int, (orig_x1, orig_y1, orig_x1 + orig_w, orig_y1 + orig_h)))
 
         tlwhbox = tuple(map(int, (x1, y1, x1 + w, y1 + h)))
-        # Crop 및 저장
-        # if frame_id == 113:
-        #     crop_img = im[intbox[1]:intbox[3], intbox[0]:intbox[2]]
-        #     crop_filename = f"{save_dir}/crop_frame{frame_id}_id{obj_ids[i]}.png"
-        #     cv2.imwrite(crop_filename, crop_img)
 
-        xyxy_color = (255, 255, 254)
+        # Crop 및 저장
+        #crop_img = im[intbox[1]:intbox[3], intbox[0]:intbox[2]]
+        #crop_filename = f"{save_dir}/crop_frame{frame_id}_id{obj_ids[i]}.png"
+        #cv2.imwrite(crop_filename, crop_img)
+
+        #xyxy_color = (255, 255, 254)
         obj_id = int(obj_ids[i])
         id_text = '{}'.format(int(obj_id))
         if ids2 is not None:
             id_text = id_text + ', {}'.format(int(ids2[i]))
         color = get_color(abs(obj_id))
-        cv2.rectangle(im, tlwhbox[0:2], tlwhbox[2:4], color=xyxy_color, thickness=1)
+        #cv2.rectangle(im, tlwhbox[0:2], tlwhbox[2:4], color=xyxy_color, thickness=1)
         cv2.rectangle(im, intbox[0:2], intbox[2:4], color=color, thickness=line_thickness)
         cv2.putText(im, id_text, (intbox[0], intbox[1]), cv2.FONT_HERSHEY_PLAIN, text_scale, (0, 0, 255),
                     thickness=text_thickness)
+
+    # crop 한 후 fps plot   
+    cv2.putText(im, 'frame: %d fps: %.2f num: %d' % (frame_id, fps, len(tlwhs)),
+                (0, int(15 * text_scale)), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), thickness=2)
+
     return im
 
 
